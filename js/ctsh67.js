9aa25f4a9d3a6db44e1b300fc236748dadcc1946
ScarWu <xneriscool@gmail.com>
2014-01-21 09:32:55 +0800

    update

--- js/ctsh67.js
+++ js/ctsh67.js
@@ -1,2 +1,13 @@
 'use strict'
 
+$(document).ready(function () {
+	$(window).on('resize', function () {
+		$('body').css({
+			'padding-top': $(window).height() + 'px'
+		});
+	});
+
+	$('body').css({
+			'padding-top': $(window).height() + 'px'
+		});
+});
